b6b73df04367744895947c051d404dc1004db34e
3arthqu4ke <143216072+34rthq04k3@users.noreply.github.com>
2024-04-11 17:25:08 -0500

    Update UpdateChecker.java

--- src/main/java/me/bebeli555/cookieclient/mods/misc/UpdateChecker.java
+++ src/main/java/me/bebeli555/cookieclient/mods/misc/UpdateChecker.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
-
 import me.bebeli555.cookieclient.Mod;
 import me.bebeli555.cookieclient.gui.Group;
 import net.minecraft.util.text.ITextComponent;
@@ -18,53 +17,56 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 //Checks the UpdateChecker.txt file in the main repository for new updates
 public class UpdateChecker extends Mod {
 	public static UpdateChecker instance;
-	public static String link = "https://github.com/Pryrobite/nhack-release/releases/download/v/";
+	public static String link = "https://github.com/34rthq04k3/Nhack/releases/download/r131/";
 	private String newVersion = null;
-	
-	public UpdateChecker() {
+
+
+
+    public UpdateChecker() {
 		super(Group.MISC, "AutoUpdate", "Checks if theres a new version of CookieClient in startup", "And notifies you in chat if you are running an outdated version");
-		this.defaultOn = true;
-		this.defaultHidden = true;
-		this.autoSubscribe = false;
+		this.defaultOn = false;
+		this.defaultHidden = false;
+		this.autoSubscribe = true;
 		instance = this;
 	}
-	
-	@Override
+
 	public boolean onEnabled() {
 		new Thread() {
 			public void run() {
 				try {
-			        URL url = new URL(link);
+					URL url = new URL(link);
 			        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
 			        String inputLine;
 			        while ((inputLine = in.readLine()) != null) {
 			            if (inputLine.contains("Mod")) {
-			    	        newVersion = inputLine.replace("Mod=", Mod.NAME + "-" + VERSION);
+			    	        newVersion = url + Mod.NAME + "-" + Mod.VERSION + ".jar";
 			    	        MinecraftForge.EVENT_BUS.register(instance);
 			    	        break;
 			            }
 			        }
-			        
-			        in.close();
+					in.readLine();
+					in.markSupported();
+					System.out.println(instance.newVersion.getBytes());
+					System.out.println(in.readLine());
+					System.out.println(in.markSupported());
+					in.close();
 				} catch (Exception ignored) {
 
 				}
 			}
 		}.start();
-        return false;
+		return true;
     }
 	
 	@SubscribeEvent
 	public void onTick(ClientTickEvent e) {
 		if (mc.player != null) {
-			if (newVersion != null && !newVersion.equals(VERSION)) {
-				String link = "https://github.com/Pryrobite/nhack-1.0/releases/download/NHACK-UPDATER/" + newVersion;
-				
+			if (newVersion != null && !newVersion.equals(Mod.NAME + "-" + Mod.VERSION + ".jar")) {
 				ITextComponent[] messages = {
-						new TextComponentString(ChatFormatting.YELLOW + "New version of " + ChatFormatting.GREEN + NAME + ChatFormatting.YELLOW + " is available!"),
-						new TextComponentString(ChatFormatting.YELLOW + "New version: " + ChatFormatting.GREEN + newVersion + ChatFormatting.YELLOW+ " Your version: " + ChatFormatting.GREEN + VERSION),
-						new TextComponentString(ChatFormatting.YELLOW + "Download it from " + ChatFormatting.AQUA + ChatFormatting.UNDERLINE + "Github")
+						new TextComponentString(ChatFormatting.BLACK + "New version of " + ChatFormatting.GREEN + NAME + ChatFormatting.YELLOW + " is available!"),
+						new TextComponentString(ChatFormatting.BLACK + "New version: " + ChatFormatting.GREEN + newVersion + ChatFormatting.YELLOW+ " Your version: " + ChatFormatting.GREEN + VERSION),
+						new TextComponentString(ChatFormatting.BLACK + "Download it from " + ChatFormatting.AQUA + ChatFormatting.UNDERLINE + "Github")
 				};
 				
 				for (ITextComponent message : messages) {
