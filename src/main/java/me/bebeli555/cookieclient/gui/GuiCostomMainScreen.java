bd8471c756c5391ce1096cdd27913dd75d3785fb
3arthqu4ke <143216072+34rthq04k3@users.noreply.github.com>
2024-04-11 17:33:09 -0500

    Create GuiCostomMainScreen.java

---
+++ src/main/java/me/bebeli555/cookieclient/gui/GuiCostomMainScreen.java
@@ -0,0 +1,82 @@
+package me.bebeli555.cookieclient.gui;
+
+
+import net.minecraft.client.gui.*;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GuiCustomMainScreen extends GuiScreen {
+    private final String backgroundURL = "https://imgur.com/a/DPdfHnZ";
+    private final ResourceLocation resourceLocation = new ResourceLocation("assets/nhack/pryrobite/textures/logo.png");
+    private int y;
+    private int x;
+    private int singleplayerWidth;
+    private int multiplayerWidth;
+    private int settingsWidth;
+    private int exitWidth;
+    private int textHeight;
+    private float xOffset;
+    private float yOffset;
+
+    public static boolean drawCompleteImage(float posX, float posY, float width, float height) {
+        GL11.glPushMatrix();
+        GL11.glTranslatef(posX, posY, 0.0f);
+        GL11.glBegin(7);
+        GL11.glTexCoord2f(0.0f, 0.0f);
+        GL11.glVertex3f(0.0f, 0.0f, 0.0f);
+        GL11.glTexCoord2f(0.0f, 1.0f);
+        GL11.glVertex3f(0.0f, height, 0.0f);
+        GL11.glTexCoord2f(1.0f, 1.0f);
+        GL11.glVertex3f(width, height, 0.0f);
+        GL11.glTexCoord2f(1.0f, 0.0f);
+        GL11.glVertex3f(width, 0.0f, 0.0f);
+        GL11.glEnd();
+        GL11.glPopMatrix();
+        return true;
+    }
+
+    public static boolean isHovered(int x, int y, int width, int height, int mouseX, int mouseY) {
+        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY < y + height;
+    }
+
+    public static void drawCompleteImage(ResourceLocation resourceLocation) {
+    }
+
+    protected void actionPerformed(GuiButton button) {
+    }
+
+
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.xOffset = -1.0f * (((float) mouseX - (float) this.width / 2.0f) / ((float) this.width / 32.0f));
+        this.yOffset = -1.0f * (((float) mouseY - (float) this.height / 2.0f) / ((float) this.height / 18.0f));
+        this.x = this.width / 2;
+        this.y = this.height / 4 + 48;
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+        this.mc.getTextureManager().bindTexture(this.resourceLocation);
+        GuiCustomMainScreen.drawCompleteImage(-16.0f + this.xOffset, -9.0f + this.yOffset, this.width + 32, this.height + 18);
+        this.mc.getTextureManager().deleteTexture(this.resourceLocation);
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    public BufferedImage parseBackground(BufferedImage background) {
+        int height;
+        int width = 1920;
+        int srcWidth = background.getWidth();
+        int srcHeight = background.getHeight();
+        for (height = 1080; width < srcWidth || height < srcHeight; width *= 2, height *= 2) {
+        }
+        BufferedImage imgNew = new BufferedImage(width, height, 2);
+        Graphics g = imgNew.getGraphics();
+        g.drawImage(background, 0, 0, null);
+        g.dispose();
+        return imgNew;
+    }
+
+}
