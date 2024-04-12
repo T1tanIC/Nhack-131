591590dbe1da612aa7a2e083e23b72164b9e12e2
3arthqu4ke <143216072+34rthq04k3@users.noreply.github.com>
2024-04-12 08:01:48 -0500

    Update Group.java

--- src/main/java/me/bebeli555/cookieclient/gui/Group.java
+++ src/main/java/me/bebeli555/cookieclient/gui/Group.java
@@ -1,16 +1,16 @@
 package me.bebeli555.cookieclient.gui;
 
 public enum Group {
-	BOTS("botting", 828, 60, "the bots"),
-	GUI("clickgui", 622, 60, "stuff about the gui and hud"),
-	GAMES("minis", 725, 60, "fun games to play"),
-	COMBAT("combat", 4, 60, "combat modules for pvp and stuff"),
-	EXPLOITS("exploit", 107, 60, "useful exploit modules"),
-	MISC("misc", 210, 60, "ther modules"),
-	MOVEMENT("movement", 313, 60, "Movement modules"),
-	RENDER("seeing", 416, 60, "client-sided render modules"),
-	WORLD("earthattack", 519, 60, "some other modules that like do something"),
-	GAMEMECHANICS("gamemechanics",700,70,"gamming");
+	BOTS("Botting", 828, 60, "the bots"),
+	GUI("Clickgui", 622, 60, "stuff about the gui and hud"),
+	GAMES("Minis", 725, 60, "fun games to play"),
+	COMBAT("Combat", 4, 60, "combat modules for pvp and stuff"),
+	EXPLOITS("Exploit", 107, 60, "useful exploit modules"),
+	MISC("Misc", 210, 60, "ther modules"),
+	MOVEMENT("Movement", 313, 60, "Movement modules"),
+	RENDER("Seeing", 416, 60, "client-sided render modules"),
+	WORLD("WorldSploit", 519, 60, "some other modules that like do something"),
+	GAMEMECHANICS("GameMechanics",700,70,"gamming");
 
     public String name;
 	public int x, y;
